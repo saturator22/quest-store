38bf045e6b2ffdf765a4265a719e1103de7ad532
Bart <bartpetka@gmail.com>
2018-05-22 22:45:42 +0200

    Implement Artifact class with getters/setters

---
+++ Quest_Store/src/main/java/com/codecool/Model/Artifact.java
@@ -0,0 +1,32 @@
+package com.codecool.Model;
+
+public class Artifact {
+    private Integer artifactId;
+    private String name;
+    private Integer price;
+    private String description;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
