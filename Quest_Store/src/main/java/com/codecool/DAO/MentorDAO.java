8443bdfce30ff1f9478edd13b7ff6c31da6dc429
Bart <bartpetka@gmail.com>
2018-05-22 16:03:49 +0200

    Create MentorDAO class

---
+++ Quest_Store/src/main/java/com/codecool/DAO/MentorDAO.java
@@ -0,0 +1,11 @@
+package com.codecool.DAO;
+
+public class MentorDAO {
+    public boolean addStudent(Student student);
+    public Student getStudent();
+    public boolean UpdateArtifact(Artifact artifact);
+    public boolean addQuest();
+    public boolean updateQuest();
+
+
+}
