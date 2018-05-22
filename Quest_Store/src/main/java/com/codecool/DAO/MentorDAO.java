636795b523eefb7e58c2001c7956cb6e92c0790f
Bart <bartpetka@gmail.com>
2018-05-22 19:11:55 +0200

    Implement addQuest method

--- Quest_Store/src/main/java/com/codecool/DAO/MentorDAO.java
+++ Quest_Store/src/main/java/com/codecool/DAO/MentorDAO.java
@@ -1,6 +1,7 @@
 package com.codecool.DAO;
 
 import com.codecool.Connection.ConnectionBuilder;
+import com.codecool.Model.Quest;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -35,13 +36,42 @@ public class MentorDAO implements DAO {
         return false;
     }
 
-    public Student getStudent(Integer userId);
+    public Student getStudent(Integer userId) {
+        // not necessary
+    }
 
-    public boolean UpdateArtifact(Artifact artifact);
+    public boolean UpdateArtifact(Artifact artifact) {
 
-    public boolean addQuest();
+    }
 
-    public boolean updateQuest(Integer questId);
+    public boolean addQuest(Quest quest) {
+        String query = "INSERT INTO quests(quest_category, quest_name, reward) VALUES (?, ?, ?)";
+
+        try {
+            Connection connection = ConnectionBuilder.getConnection();
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, quest.getQuestCategory());
+            ps.setString(2, quest.getQuestName());
+            ps.setInt(3, quest.getQuestReward());
+
+            int i = ps.executeUpdate();
+
+            ps.close();
+            connection.close();
+
+            if (i == 1) {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean updateQuest(Integer questId){
+        
+    }
 
 
 }
