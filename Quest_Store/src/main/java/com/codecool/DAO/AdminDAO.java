0a1e2ed75b7c36cacc812d5e0c1d88cc201cbd5c
Roman <adam.romanowski93@gmail.com>
2018-05-23 13:37:38 +0200

    Implement addClass method() with updateQuerySuccessful() private mehtod;

--- Quest_Store/src/main/java/com/codecool/DAO/AdminDAO.java
+++ Quest_Store/src/main/java/com/codecool/DAO/AdminDAO.java
@@ -1,14 +1,45 @@
 package com.codecool.DAO;
 
+import com.codecool.Connection.ConnectionBuilder;
 import com.codecool.Model.Mentor;
 
-public class AdminDAO implements DAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AdminDAO extends DAO {
 
     public boolean addMentor(Mentor mentor) {
         boolean isDone = addUserData(mentor);
         return isDone;
     }
 
-    
+    public boolean addClass(ClassRoom classRoom) {
+        String query = "INSERT INTO Classes(name) VALUES(?)";
+        boolean updateSuccessful = false;
+
+        try {
+            Connection connection = ConnectionBuilder.getConnection();
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, classRoom.getName());
+
+            int intValue = ps.executeUpdate();
+            updateSuccessful = updateQuerySuccessful(intValue);
+
+            ps.close();
+            connection.close();
+
+            if (updateSuccessful) { return true; }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return updateSuccessful;
+    }
+
+    private boolean updateQuerySuccessful(int intValue) {
+        if (intValue == 1) { return true; }
+        return false;
+    }
 
 }
