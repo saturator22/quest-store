f76680c586d3616c3d6a3901c494c0e1962731a0
Roman <adam.romanowski93@gmail.com>
2018-05-24 11:54:07 +0200

    Implement addMentor() method

--- Quest_Store/src/main/java/com/codecool/Controller/AdminController.java
+++ Quest_Store/src/main/java/com/codecool/Controller/AdminController.java
@@ -4,7 +4,19 @@ import com.codecool.Model.Mentor;
 import com.codecool.Model.User;
 import com.codecool.input.UserInput;
 
-public class AdminController {
+public class AdminController implements IUserCreateable {
 
+    private static final int MENTOR_ROLE = 2;
+
+    public void addMentor() {
+        UserInput userInput = new UserInput();
+        User mentor = new Mentor();
+        mentor = setUserAttibutes(mentor);
+        mentor.setRoleId(MENTOR_ROLE);
+
+        mentor = (Mentor) mentor;
+        MentorDAO mentorDAO = new MentorDAO();
+        mentorDAO.addMentor(mentor);
+    }
 
 }
