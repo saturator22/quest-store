89f0f52f1c51570c8a36a5ec92e3cb7b4d10754e
Roman <adam.romanowski93@gmail.com>
2018-05-24 11:03:08 +0200

    Add setUserAttributes() method

--- Quest_Store/src/main/java/com/codecool/Controller/AdminController.java
+++ Quest_Store/src/main/java/com/codecool/Controller/AdminController.java
@@ -1,4 +1,14 @@
 package com.codecool.Controller;
 
+import com.codecool.Model.Mentor;
+import com.codecool.Model.User;
+import com.codecool.input.UserInput;
+
 public class AdminController {
+
+    private User createUser(User user) {
+        UserInput userInput = new UserInput();
+
+    }
+
 }
