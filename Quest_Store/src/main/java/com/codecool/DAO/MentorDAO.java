c63946b79dc2dab165386f83f2b7ce8c335cad6b
Bart <bartpetka@gmail.com>
2018-05-24 17:42:07 +0200

    Implement insertMentorData method

--- Quest_Store/src/main/java/com/codecool/DAO/MentorDAO.java
+++ Quest_Store/src/main/java/com/codecool/DAO/MentorDAO.java
@@ -4,6 +4,8 @@ import com.codecool.Connection.ConnectionBuilder;
 import com.codecool.Model.Mentor;
 import com.codecool.Model.User;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -25,5 +27,35 @@ public class MentorDAO extends DAO {
 
     public User extractUserFromRow(ResultSet resultSet) throws SQLException {
         return extractMentor(resultSet);
+    }
+
+    public boolean insertMentorData(Mentor mentor) {
+        String addPersonalData = "INSERT INTO users (role_id, first_name, last_name, login, email, password)\n" +
+                "VALUES (" + mentor.getRoleId() + ", '" + mentor.getFirstName() + "', '" + mentor.getLastName() + "', '" +
+                mentor.getLogin() + "', '" + mentor.getEmail() + "', " +
+                mentor.getPassword() + ")\n" +
+                "ON CONFLICT DO NOTHING;\n";
+
+        String addMentorData = "INSERT INTO mentors (user_id)\n" +
+                "VALUES ((SELECT user_id FROM users WHERE login = '" + mentor.getLogin() + "'))\n" +
+                "ON CONFLICT DO NOTHING;";
+
+        try {
+            Connection connection = ConnectionBuilder.getConnection();
+            PreparedStatement ps = connection.prepareStatement(addPersonalData + addMentorData);
+
+            int i = ps.executeUpdate();
+
+            ps.close();
+            connection.close();
+
+            if (i == 1) {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
