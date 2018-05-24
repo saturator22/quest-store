eee31fdefa91f3f7bed036d01ef7755d051cadf4
Bart <bartpetka@gmail.com>
2018-05-24 19:02:03 +0200

    Implement getQuestById method

--- Quest_Store/src/main/java/com/codecool/DAO/QuestDAO.java
+++ Quest_Store/src/main/java/com/codecool/DAO/QuestDAO.java
@@ -9,13 +9,37 @@ import java.util.List;
 
 public class QuestDAO {
 
-    public List<Quest> getStudentQuests(String ownerId) {
+    public Quest getQuestByQuestId(Integer questId) {
+        String query = "SELECT * FROM quests WHERE quest_id =" + questId;
+        Quest quest = null;
+
+        try {
+            Connection connection = ConnectionBuilder.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                quest = extractQuest(resultSet);
+            }
+
+            statement.close();
+            resultSet.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return quest;
+    }
+
+    public List<Quest> getStudentQuests(Integer ownerId) {
         List<Quest> studentQuests = new ArrayList<>();
 
         String query = "SELECT quests.*, students_quests.user_id " +
                 "FROM quests JOIN students_quests " +
                 "ON students_quests.quest_id = quests.quest_id " +
-                "JOIN quests ON quests.user_id = students_quests.user_id WHERE students_quests.user_id = " + ownerId;
+                "WHERE students_quests.user_id = " + ownerId;
 
         try {
             Connection connection = ConnectionBuilder.getConnection();
@@ -45,7 +69,7 @@ public class QuestDAO {
         quest.setQuestName(resultSet.getString("quest_name"));
         quest.setQuestReward(resultSet.getInt("quest_reward"));
         quest.setQuestCategory(resultSet.getString("quest_category"));
-        quest.setQuestOwnerId(resultSet.getInt("user_id"));
+//        quest.setQuestOwnerId(resultSet.getInt("user_id"));
 
         return quest;
     }
@@ -85,4 +109,7 @@ public class QuestDAO {
 
         return sendQuestQuery(query, quest);
     }
+
+
+
 }
