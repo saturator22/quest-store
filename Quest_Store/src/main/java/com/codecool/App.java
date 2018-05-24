1e836c627073927808f8708bb35b3f165c012b5b
Roman <adam.romanowski93@gmail.com>
2018-05-24 17:14:14 +0200

    Implement main for tests

--- Quest_Store/src/main/java/com/codecool/App.java
+++ Quest_Store/src/main/java/com/codecool/App.java
@@ -1,8 +1,10 @@
 package com.codecool;
 
+import com.codecool.Controller.AdminController;
 import com.codecool.DAO.DAO;
 import com.codecool.Model.Mentor;
 import com.codecool.Model.User;
+import com.codecool.input.UserInput;
 
 /**
  * Hello world!
@@ -12,14 +14,39 @@ public class App
 {
     public static void main( String[] args )
     {
-        User mentor = new Mentor();
-        mentor.setRoleId(1);
-        mentor.setFirstName("MILOSZ");
-        mentor.setLastName("Romanowski");
-        mentor.setLogin("bartp");
-        mentor.setPassword("tarara");
-        mentor.setEmail("wfwrfw@wesd.com");
+        UserInput userInput = new UserInput();
+        int option = -1;
 
-        DAO.addUserData(mentor);
+        while (option != 0) {
+            System.out.println("1. Admin: create mentor account\n" +
+                    "2. Admin: check mentor account\n" +
+                    "3. Admin: create class\n" +
+                    "4. Admin: set lvl experience\n" +
+                    "---------------------------------------\n" +
+                    "5. Mentor: add Student\n" +
+                    "6. Mentor: create Quest\n" +
+                    "7. Mentor: create artifact\n" +
+                    "8. Mentor: update artifact\n" +
+                    "9. Mentor: add artifact to store\n" +
+                    "10. Mentor: set artifact category\n" +
+                    "---------------------------------------\n" +
+                    "11. Student: see exp lvl" +
+                    "12. Student: see account info\n");
+            option = userInput.getInt("Enter option: ");
+
+            if (option == 1) {
+                AdminController adminController = new AdminController();
+                adminController.addMentor();
+            } else if (option == 2) {
+                AdminController adminController = new AdminController();
+                adminController.viewMentorAccount();
+            } else if (option == 3) {
+                AdminController adminController = new AdminController();
+                adminController.addClass();
+            } else if (option == 4) {
+                AdminController adminController = new AdminController();
+                adminController.addLevel();
+            }
+        }
     }
 }
