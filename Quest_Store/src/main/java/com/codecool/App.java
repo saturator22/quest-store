235065a12deb831eeeb30a08dac6754f4118290a
Bart <bartpetka@gmail.com>
2018-05-25 17:01:26 +0200

    clear testing app.java

--- Quest_Store/src/main/java/com/codecool/App.java
+++ Quest_Store/src/main/java/com/codecool/App.java
@@ -1,25 +1,6 @@
 package com.codecool;
 
-import com.codecool.DAO.DAO;
-import com.codecool.Model.Mentor;
-import com.codecool.Model.User;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        User mentor = new Mentor();
-        mentor.setRoleId(1);
-        mentor.setFirstName("MILOSZ");
-        mentor.setLastName("Romanowski");
-        mentor.setLogin("bartp");
-        mentor.setPassword("tarara");
-        mentor.setEmail("wfwrfw@wesd.com");
-
-        DAO.addUserData(mentor);
+public class App {
+    public static void main( String[] args ) {
     }
 }
