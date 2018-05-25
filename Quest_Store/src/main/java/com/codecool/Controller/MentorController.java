da102247822a4ce38c4c74651cfe21ea4d115ca4
Roman <adam.romanowski93@gmail.com>
2018-05-25 10:17:43 +0200

    Add setting starting lvl as default for Student

--- Quest_Store/src/main/java/com/codecool/Controller/MentorController.java
+++ Quest_Store/src/main/java/com/codecool/Controller/MentorController.java
@@ -12,6 +12,7 @@ import com.codecool.input.UserInput;
 public class MentorController implements IUserCreateable {
 
     private static final int STUDENT_ROLE = 3;
+    private static final int STARTING_LVL = 1;
 
     public void addStudent() {
         UserInput userInput = new UserInput();
@@ -23,6 +24,7 @@ public class MentorController implements IUserCreateable {
         Student student = (Student) user;
         student.setClassId(userInput.getInt("Enter class id: "));
         student.setGithub(userInput.getString("Enter github: "));
+        student.setLevelId(STARTING_LVL);
 
         StudentDAO studentDAO = new StudentDAO();
         studentDAO.insertStudentData(student);
