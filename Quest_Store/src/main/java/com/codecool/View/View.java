33a02f6b14dec83677490d481daed4ee656b8d73
Bart <bartpetka@gmail.com>
2018-05-26 16:44:46 +0200

    Implement selectShopObject method

--- Quest_Store/src/main/java/com/codecool/View/View.java
+++ Quest_Store/src/main/java/com/codecool/View/View.java
@@ -2,7 +2,10 @@ package com.codecool.View;
 
 import com.codecool.Model.User;
 import com.codecool.Model.ShopObject;
+import com.codecool.input.UserInput;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class View {
@@ -15,6 +18,27 @@ public class View {
         for (ShopObject quest : setOfItems) {
             System.out.println(quest);
         }
+    }
+    
+    public static ShopObject selectShopObject(List<ShopObject> availableObjects) {
+        int i = 1;
+
+        ShopObject artifact = null;
+
+        for (ShopObject art : availableObjects) {
+            System.out.println(i + ": " + art);
+            i++;
+        }
+
+        UserInput ui = new UserInput();
+        int userChoice = ui.getInt("Select artifact: ");
+
+        try {
+            artifact = availableObjects.get(userChoice - 1);
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+        return artifact;
     }
 
 }
