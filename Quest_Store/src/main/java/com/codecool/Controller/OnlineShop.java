c8da84025ae72b6df0528c2aae4f008a4cc910cb
Bart <bartpetka@gmail.com>
2018-05-26 16:46:20 +0200

    Add currentAccount field with setter/getter

--- Quest_Store/src/main/java/com/codecool/Controller/OnlineShop.java
+++ Quest_Store/src/main/java/com/codecool/Controller/OnlineShop.java
@@ -2,11 +2,26 @@ package com.codecool.Controller;
 
 import com.codecool.DAO.ArtifactDAO;
 import com.codecool.DAO.QuestDAO;
+import com.codecool.Model.Artifact;
 import com.codecool.Model.ShopObject;
+import com.codecool.Model.Student;
+import com.codecool.input.UserInput;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class OnlineShop {
+    private Student currentAccount;
+
+    public void setCurrenctAccount(Student currenctAccount) {
+        this.currentAccount = currenctAccount;
+    }
+
+    public Student getCurrenctAccount() {
+        return currentAccount;
+    }
+
     public Set<ShopObject> getAvailableQuests() {
         QuestDAO questDao = new QuestDAO();
         return questDao.getAvailableQuests();
@@ -16,4 +31,7 @@ public class OnlineShop {
         ArtifactDAO artifactDAO = new ArtifactDAO();
         return artifactDAO.getAvailableArtifacts();
     }
+
+
+
 }
