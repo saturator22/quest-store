823f20d616aa92ed7c9968294249d8fd5cd630d4
Bart <bartpetka@gmail.com>
2018-05-26 13:46:20 +0200

    Implement questStore testing menu

--- Quest_Store/src/main/java/com/codecool/App.java
+++ Quest_Store/src/main/java/com/codecool/App.java
@@ -2,7 +2,13 @@ package com.codecool;
 
 import com.codecool.Controller.AdminController;
 import com.codecool.Controller.MentorController;
+import com.codecool.Controller.OnlineShop;
+import com.codecool.Model.Quest;
+import com.codecool.View.View;
 import com.codecool.input.UserInput;
+
+import javax.swing.text.html.Option;
+import java.util.Set;
 
 public class App {
 
@@ -13,7 +19,8 @@ public class App {
         int option = -1;
 
         while (option != 0) {
-            System.out.println("1. Admin: create mentor account\n" +
+            System.out.println("0. Quest Store\n" +
+                    "1. Admin: create mentor account\n" +
                     "2. Admin: check mentor account\n" +
                     "3. Admin: create class\n" +
                     "4. Admin: set lvl experience !! IN PROGRESS\n" +
@@ -26,7 +33,8 @@ public class App {
                     "10. Mentor: set quest category !! IN PROGRESS\n" +
                     "---------------------------------------\n" +
                     "11. Student: see exp lvl !! IN PROGRESS\n" +
-                    "12. Student: see account info !! IN PROGRESS\n");
+                    "12. Student: see account info !! IN PROGRESS\n" +
+                    "\n");
             option = userInput.getInt("Enter option: ");
 
             if (option == 1) {
@@ -56,6 +64,39 @@ public class App {
             } else if (option == 9) {
                 MentorController mentorController = new MentorController();
                 mentorController.setQuestCategory();
+            } else if (option == 0) {
+                testShopMenu();
+            }
+        }
+    }
+
+    public static void testShopMenu() {
+        boolean continueShopping = true;
+        OnlineShop onlineShop = new OnlineShop();
+        UserInput userInput = new UserInput();
+
+        while(continueShopping) {
+            System.out.println("------ONLINE SHOP------\n\n 1. See all items \n 2. See quests \n 3. See artifacts \n 4. Exit");
+            String userChoice = userInput.getString("Select option: ");
+
+            if (userChoice.equals("1")) {
+                System.out.println("Available items: ");
+                View.displaySetOfItems(onlineShop.getAvailableArtifacts());
+                View.displaySetOfItems(onlineShop.getAvailableQuests());
+
+            } else if (userChoice.equals("2")) {
+                System.out.println("Available quests: ");
+                View.displaySetOfItems(onlineShop.getAvailableQuests());
+
+            } else if (userChoice.equals("3")) {
+                System.out.println("Available artifacts: ");
+                View.displaySetOfItems(onlineShop.getAvailableArtifacts());
+
+            } else if (userChoice.equals("4")) {
+                continueShopping = false;
+
+            } else {
+                System.out.println("Wrong selection...");
             }
         }
     }
