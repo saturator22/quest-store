b7003066c5ef9afc0d3ecf82b99773d38528e6cd
Bart <bartpetka@gmail.com>
2018-06-18 21:51:23 +0200

    Create StudentHandler

---
+++ Quest_Store/src/main/java/com/codecool/Handlers/StudentHandler.java
@@ -0,0 +1,25 @@
+package com.codecool.Handlers;
+
+import com.codecool.DAO.ArtifactDAO;
+import com.codecool.DAO.StudentDAO;
+import com.codecool.Model.Artifact;
+import com.codecool.Model.Student;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        System.out.println("Not implemented yet");
+    }
+
+
+}
