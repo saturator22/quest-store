3c9e07bf3d9820532b2fd48be391bfc4e211a04a
Bart <bartpetka@gmail.com>
2018-06-19 15:44:29 +0200

    Delete target directory

--- Quest_Store/target/classes/static/js/toggleInventory.js
+++
@@ -1,7 +0,0 @@
-var toggleInventory = function () {
-  var mydiv = document.getElementById('inventory');
-  if (mydiv.style.display === 'flex' || mydiv.style.display === '')
-    mydiv.style.display = 'none';
-  else
-    mydiv.style.display = 'flex'
-}
--- Quest_Store/target/classes/static/js/toggleStats.js
+++
@@ -1,7 +0,0 @@
-var toggleStats = function () {
-    var mydiv = document.getElementById('character');
-    if (mydiv.style.display === 'none' || mydiv.style.display === '')
-      mydiv.style.display = 'flex';
-    else
-      mydiv.style.display = 'none'
-  }
