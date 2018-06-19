e5eb2806174558e5bd4cf0f1db223a385caef73f
Bart <bartpetka@gmail.com>
2018-06-19 13:33:49 +0200

    Rebuild app for UX display

--- Quest_Store/src/main/java/com/codecool/App.java
+++ Quest_Store/src/main/java/com/codecool/App.java
@@ -7,101 +7,111 @@ import com.codecool.Controller.OnlineShop;
 import com.codecool.Controller.StudentController;
 import com.codecool.DAO.MentorDAO;
 import com.codecool.DAO.StudentDAO;
+import com.codecool.Handlers.StaticHandler;
+import com.codecool.Handlers.StudentHandler;
 import com.codecool.Model.*;
 import com.codecool.DAO.StudentDAO;
 import com.codecool.Model.Student;
 import com.codecool.View.View;
 import com.codecool.input.UserInput;
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 
 public class App {
 
 
-    public static void main( String[] args )
-    {
-        UserInput userInput = new UserInput();
-        int option = -1;
-
-        while (option != 0) {
-            System.out.println("0. Quest Store\n" +
-                    "1. Admin: create mentor account\n" +
-                    "2. Admin: check mentor account\n" +
-                    "3. Admin: create class\n" +
-                    "4. Admin: set lvl experience !! IN PROGRESS\n" +
-                    "---------------------------------------\n" +
-                    "5. Mentor: add Student !! IN PROGRESS\n" +
-                    "6. Mentor: create Quest\n" +
-                    "7. Mentor: create artifact\n" +
-                    "8. Mentor: update artifact\n" +
-                    "9. Mentor: add artifact to store !! IN PROGRESS\n" +
-                    "10. Mentor: set quest category !! IN PROGRESS\n" +
-                    "---------------------------------------\n" +
-                    "11. Student: see exp lvl !! IN PROGRESS\n" +
-                    "12. Student: see account info !! IN PROGRESS\n"+
-                    "13. Student: find Student by ID\n" +
-                    "14. Student: Create CrowdFund" +
-                    "15. Mentor: Add Mentor to class + print mentor classes: ");
-            option = userInput.getInt("Enter option: ");
-
-            if (option == 1) {
-                AdminController adminController = new AdminController();
-                adminController.addMentor();
-            } else if (option == 2) {
-                AdminController adminController = new AdminController();
-                adminController.viewMentorAccount();
-            } else if (option == 3) {
-                AdminController adminController = new AdminController();
-                adminController.addClass();
-            } else if (option == 4) {
-                AdminController adminController = new AdminController();
-//                adminController.addLevel(); TODO
-            } else if (option == 5) {
-                MentorController mentorController = new MentorController();
-                mentorController.addStudent();
-            } else if (option == 6) {
-                MentorController mentorController = new MentorController();
-                mentorController.addQuest();
-            } else if (option == 7) {
-                MentorController mentorController = new MentorController();
-                mentorController.addArtifact();
-            } else if (option == 8) {
-                MentorController mentorController = new MentorController();
-                mentorController.updateArtifact();
-            } else if (option == 9) {
-                MentorController mentorController = new MentorController();
-                mentorController.setQuestCategory();
-            } else if (option == 13) {
-                StudentDAO studentDAO = new StudentDAO();
-                List<Student> list = studentDAO.getStudents();
-                for(Student student: list) {
-                    System.out.println(student);
-                }
-                int studentID = userInput.getInt("Type Id: ");
-                Student student = studentDAO.getStudentById(studentID);
-                System.out.println(student);
-            } else if (option == 14){
-                StudentController studentController = new StudentController();
-                List<Student> studentList = studentController.getCrowdFundStudents();
-                for (Student student: studentList) {
-                    System.out.println(student);
-                }
-                int groupBalance = studentController.sumGroupShoppersBalance(studentList);
-                System.out.println("Group Balance: " + groupBalance);
-            } else if (option == 15) {
-                MentorDAO mentorDAO = new MentorDAO();
-                mentorDAO.addMentorToClassRoom(mentorDAO.getMentorById(21), 2);
-                MentorController mentorController = new MentorController();
-                List<ClassRoom> mentorClassRooms = mentorController.getMentorsClassRooms();
-                for(ClassRoom classRoom: mentorClassRooms) {
-                    System.out.println(classRoom);
-                }
-            }else if (option == 0) {
-                testShopMenu();
-            }
-        }
+    public static void main( String[] args ) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/questStore", new StudentHandler());
+        server.createContext("/static", new StaticHandler());
+        server.setExecutor(null);
+        server.start();
     }
+//        UserInput userInput = new UserInput();
+//        int option = -1;
+//
+//        while (option != 0) {
+//            System.out.println("0. Quest Store\n" +
+//                    "1. Admin: create mentor account\n" +
+//                    "2. Admin: check mentor account\n" +
+//                    "3. Admin: create class\n" +
+//                    "4. Admin: set lvl experience !! IN PROGRESS\n" +
+//                    "---------------------------------------\n" +
+//                    "5. Mentor: add Student !! IN PROGRESS\n" +
+//                    "6. Mentor: create Quest\n" +
+//                    "7. Mentor: create artifact\n" +
+//                    "8. Mentor: update artifact\n" +
+//                    "9. Mentor: add artifact to store !! IN PROGRESS\n" +
+//                    "10. Mentor: set quest category !! IN PROGRESS\n" +
+//                    "---------------------------------------\n" +
+//                    "11. Student: see exp lvl !! IN PROGRESS\n" +
+//                    "12. Student: see account info !! IN PROGRESS\n"+
+//                    "13. Student: find Student by ID\n" +
+//                    "14. Student: Create CrowdFund" +
+//                    "15. Mentor: Add Mentor to class + print mentor classes: ");
+//            option = userInput.getInt("Enter option: ");
+//
+//            if (option == 1) {
+//                AdminController adminController = new AdminController();
+//                adminController.addMentor();
+//            } else if (option == 2) {
+//                AdminController adminController = new AdminController();
+//                adminController.viewMentorAccount();
+//            } else if (option == 3) {
+//                AdminController adminController = new AdminController();
+//                adminController.addClass();
+//            } else if (option == 4) {
+//                AdminController adminController = new AdminController();
+//                adminController.addLevel(); TODO
+//            } else if (option == 5) {
+//                MentorController mentorController = new MentorController();
+//                mentorController.addStudent();
+//            } else if (option == 6) {
+//                MentorController mentorController = new MentorController();
+//                mentorController.addQuest();
+//            } else if (option == 7) {
+//                MentorController mentorController = new MentorController();
+//                mentorController.addArtifact();
+//            } else if (option == 8) {
+//                MentorController mentorController = new MentorController();
+//                mentorController.updateArtifact();
+//            } else if (option == 9) {
+//                MentorController mentorController = new MentorController();
+//                mentorController.setQuestCategory();
+//            } else if (option == 13) {
+//                StudentDAO studentDAO = new StudentDAO();
+//                List<Student> list = studentDAO.getStudents();
+//                for(Student student: list) {
+//                    System.out.println(student);
+//                }
+//                int studentID = userInput.getInt("Type Id: ");
+//                Student student = studentDAO.getStudentById(studentID);
+//                System.out.println(student);
+//            } else if (option == 14){
+//                StudentController studentController = new StudentController();
+//                List<Student> studentList = studentController.getCrowdFundStudents();
+//                for (Student student: studentList) {
+//                    System.out.println(student);
+//                }
+//                int groupBalance = studentController.sumGroupShoppersBalance(studentList);
+//                System.out.println("Group Balance: " + groupBalance);
+//            } else if (option == 15) {
+//                MentorDAO mentorDAO = new MentorDAO();
+//                mentorDAO.addMentorToClassRoom(mentorDAO.getMentorById(21), 2);
+//                MentorController mentorController = new MentorController();
+//                List<ClassRoom> mentorClassRooms = mentorController.getMentorsClassRooms();
+//                for(ClassRoom classRoom: mentorClassRooms) {
+//                    System.out.println(classRoom);
+//                }
+//            }else if (option == 0) {
+//                testShopMenu();
+//            }
+//        }
+//    }
 
 
     public static void testShopMenu() {
