2430495a375366f14aeb606c5bc9f9af617f677b
Bart <bartpetka@gmail.com>
2018-06-19 10:35:46 +0200

    Delete unnecessary files

--- questStore/assets/js/toggleStats.js
+++
@@ -1,7 +0,0 @@
-var toggleStats = function () {
-    var mydiv = document.getElementById('character');
-    if (mydiv.style.display === 'none' || mydiv.style.display === '')
-      mydiv.style.display = 'block';
-    else
-      mydiv.style.display = 'none'
-  }
