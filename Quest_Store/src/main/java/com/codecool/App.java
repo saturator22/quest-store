0cca33a7ae16aeb3b1e76f91c7da74cae076576f
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-06-20 14:08:30 +0200

    Add new URIs in App

--- Quest_Store/src/main/java/com/codecool/App.java
+++ Quest_Store/src/main/java/com/codecool/App.java
@@ -1,13 +1,9 @@
 package com.codecool;
 
-import com.codecool.Handlers.MentorHandler;
-import com.codecool.Handlers.StaticHandler;
+import com.codecool.Handlers.*;
 import com.sun.net.httpserver.HttpServer;
 import com.codecool.Handlers.StaticHandler;
-import com.codecool.Handlers.StudentHandler;
-import com.sun.net.httpserver.HttpServer;
 
-import java.net.InetSocketAddress;
 import java.net.InetSocketAddress;
 
 public class App {
@@ -16,12 +12,15 @@ public class App {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         // set routes
-//        server.createContext("/dashboard", new Dashboard());
-        server.createContext("/dashboard", new MentorHandler());
+        server.createContext("/", new DashboardHandler());
+        server.createContext("/dashboard", new DashboardHandler());
+        server.createContext("/store", new StoreHandler());
+        server.createContext("/login", new AuthHandler());
+        server.createContext("/admin", new AdminHandler());
+        server.createContext("/mentor", new MentorHandler());
         server.createContext("/static", new StaticHandler());
         server.createContext("/questStore", new StudentHandler());
         server.setExecutor(null); // creates a default executor
-
         // start listening
         server.start();
 
