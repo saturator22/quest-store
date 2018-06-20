3708a96e44e971a86a9d8a11b01888b32e5b3d70
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-06-20 14:04:47 +0200

    Add & implement oveloaded getLoginData() in LoginDAO

--- Quest_Store/src/main/java/com/codecool/DAO/LoginDAO.java
+++ Quest_Store/src/main/java/com/codecool/DAO/LoginDAO.java
@@ -35,4 +35,37 @@ public class LoginDAO {
         }
         return loginData;
     }
+
+    public LoginData getLoginData(Integer id) {
+        LoginData loginData = null;
+
+        String query = "SELECT user_id, role_id, login, email, password FROM users WHERE user_id = ?;";
+
+        try {
+            Connection connection = ConnectionBuilder.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()) {
+                loginData = new LoginData(
+                        resultSet.getInt("role_id"),
+                        resultSet.getInt("user_id"),
+                        resultSet.getString("login"),
+                        resultSet.getString("password")
+                );
+                connection.close();
+                preparedStatement.close();
+                return loginData;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getSQLState());
+        }
+        return loginData;
+    }
+
+//    private LoginData deserialize(ResultSet rs) {
+            //TODO: Implement!!!
+//    }
 }
