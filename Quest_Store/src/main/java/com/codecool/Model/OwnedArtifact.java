c2e44cfb3533b263338a68b9f66c216545895d68
Bart <bartpetka@gmail.com>
2018-06-21 11:36:24 +0200

    Implement status field

--- Quest_Store/src/main/java/com/codecool/Model/OwnedArtifact.java
+++ Quest_Store/src/main/java/com/codecool/Model/OwnedArtifact.java
@@ -2,6 +2,15 @@ package com.codecool.Model;
 
 public class OwnedArtifact extends Artifact{
     public Integer uniqueId;
+    public Integer status;
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
 
     public void setUniqueId(Integer uniqueId) {
         this.uniqueId = uniqueId;
