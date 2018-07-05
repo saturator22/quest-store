348a8bda5099fdc628e71aa03e67103b68ee447c
Kubset <kuba1228334@gmail.com>
2018-07-05 13:23:45 +0200

    add updateArtifact test

--- Quest_Store/src/tests/com/codecool/DAO/ArtifactDAOTest.java
+++ Quest_Store/src/tests/com/codecool/DAO/ArtifactDAOTest.java
@@ -2,8 +2,7 @@ package com.codecool.DAO;
 
 import com.codecool.Model.Artifact;
 import com.codecool.Model.ShopObject;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Assertions.*;
 import org.mockito.Mock;
 
@@ -20,14 +19,18 @@ import static org.mockito.Mockito.when;
 public class ArtifactDAOTest {
 
 
-    @DisplayName("Test adding artifact")
+    @AfterEach
+    public void cleanUp() {
+        ArtifactDAO artifactDAO = new ArtifactDAO();
+        List<Artifact> artifacts = artifactDAO.getAvailableArtifacts();
+        artifactDAO.deleteArtifact(artifacts.get(0));
+    }
+
+    @DisplayName("Test add artifact")
     @Test
     public void addArtifactTest() {
-        Artifact artifact = new Artifact();
-        artifact.setName("Test name");
-        artifact.setDescription("Test description");
-        artifact.setPrice(100);
 
+        Artifact artifact = createArtifact();
         ArtifactDAO artifactDAO = new ArtifactDAO();
 
         int artifactsBefore = artifactDAO.getAvailableArtifacts().size();
@@ -38,18 +41,52 @@ public class ArtifactDAOTest {
         artifacts.stream().filter(c -> c.getName().equals("Test name"))
                           .filter(c -> c.getDescription().equals("Test description"))
                           .filter(c -> c.getPrice().equals(100));
-        List<Artifact> actualArtifact = new ArrayList<>(artifacts);
 
 
         assertAll(() -> {
-            assertEquals((int) actualArtifact.get(0).getPrice(), 100);
-            assertEquals(actualArtifact.get(0).getName(), "Test name");
-            assertEquals(actualArtifact.get(0).getDescription(), "Test description");
+            assertEquals((int) artifacts.get(0).getPrice(), 100);
+            assertEquals(artifacts.get(0).getName(), "Test name");
+            assertEquals(artifacts.get(0).getDescription(), "Test description");
             assertEquals(artifactsBefore + 1, artifactsAfter);
         });
 
+    }
 
+    @DisplayName("Test edit artifact")
+    @Test
+    public void updateArtifactTest() {
+        Artifact artifact = createArtifact();
+        ArtifactDAO artifactDAO = new ArtifactDAO();
+        artifactDAO.addArtifact(artifact);
+
+        List<Artifact> artifacts = artifactDAO.getAvailableArtifacts();
+        artifacts.stream().filter(c -> c.getName().equals("Test name"))
+                .filter(c -> c.getDescription().equals("Test description"))
+                .filter(c -> c.getPrice().equals(100));
+
+        Artifact updateArtifact = artifacts.get(0);
+        updateArtifact.setPrice(200);
+        updateArtifact.setDescription("Updated description");
+        updateArtifact.setName("Updated name");
+
+        artifactDAO.updateArtifact(updateArtifact);
+
+        assertAll(() -> {
+            assertEquals((int) artifacts.get(0).getPrice(), 200);
+            assertEquals(artifacts.get(0).getName(), "Updated name");
+            assertEquals(artifacts.get(0).getDescription(), "Updated description");
+        });
 
 
     }
+
+    private Artifact createArtifact() {
+        Artifact artifact = new Artifact();
+        artifact.setName("Test name");
+        artifact.setDescription("Test description");
+        artifact.setPrice(100);
+
+        return artifact;
+    }
+
 }
